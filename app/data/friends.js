3637bdf429d651609850b56be4b1f29f6fc34588
Hecate <fioriecheyenne@gmail.com>
2019-05-10 19:42:04 -0700

    friends, api

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends [
+    {
+        name: "Bellatrix Lestrange",
+        photo: "https://vignette.wikia.nocookie.net/harrypotter/images/1/14/BellatrixLestrange.png/revision/latest?cb=20171101034243",
+        scores: []
+    },
+    {
+        name: "Louise Belcher",
+        photo: "https://media3.giphy.com/media/tlmYrzj5Are7e/giphy.gif",
+        scores: []
+    },
+    {
+        name: "Daenerys Targaryen",
+        photo: "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2017/11/got-110-h_2017.jpg",
+        scores: []
+    },
+    {
+        name: "Jon Snow",
+        photo: "https://pbs.twimg.com/profile_images/901947348699545601/hqRMHITj_400x400.jpg",
+        scores: []
+    },
+
+    {
+        name: "Bob Belcher",
+        photo: "https://vignette.wikia.nocookie.net/bobs-burgers-fanon/images/5/51/Bob_with_a_burger.png/revision/latest?cb=20170920181424",
+        scores: []
+    },
+    {
+        name: "Arya Stark",
+        photo: "https://i.pinimg.com/originals/a3/36/a2/a336a2b718ff5761670ab106ad070a00.jpg",
+        scores: []
+    }
+];
+module.exports = friends; 

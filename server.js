7c335d93b67918951ff0097e0d2aa49ebe3c3f3a
Hecate <fioriecheyenne@gmail.com>
2019-05-10 22:50:53 -0700

    cleaning up files

--- server.js
+++ server.js
@@ -14,19 +14,5 @@ app.use(bodyParser.text());
 
 require(path.join(__dirname, "./app/routing/api"))(app);
 require(path.join(__dirname, "./app/routing/html"))(app);
-// const routes = {
-// require("./app/routing/api")(app),
-//     require("./app/routing/html")(app)
-// };
-
-// app.get("/*", (req, resp) => {
-//   if (req.url.startsWith("/api")) {
-//     routes.api.get(req, resp);
-//   } else {
-//     routes.html(req, resp);
-//   }
-// });
-
-// app.post("/api", routes.api.post);
 
 app.listen(PORT, () => console.log("Running on http://localhost: " + PORT));

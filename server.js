b1754c929ee0405d2450211bc58a3f8197565be4
Hecate <fioriecheyenne@gmail.com>
2019-05-13 17:11:12 -0700

    fixing

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "app/public")));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
 
 require(path.join(__dirname, "./app/routing/api"))(app);
 require(path.join(__dirname, "./app/routing/html"))(app);
